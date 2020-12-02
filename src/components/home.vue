e63065ee3e578fe38aeb6a39e3799b732d86e5cc
koala.Q <koala100512@sina.com>
2020-12-03 00:48:10 +0800

    ..

---
+++ src/components/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>雪莲老师的项目</div>
+</template>
+
+<script>
+
+export default {
+    name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/nav.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <ul>
+          <li>Home</li>
+          <li>Company</li>
+          <li>Steel Product</li>
+          <li>Hardware</li>
+          <li>Machinery</li>
+          <li>Contact Us</li>
+          <li>Join Us</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
